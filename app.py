ada85630d88a20ea248996b64f3fbee50f513309
okellogabrielinnocent@gmail.com <okellogabrielinnocent@gmail.com>
2018-10-03 12:20:21 +0300

    Add user login [Delivers #160949159]

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ def main():
     user = User(role,username,password)
     user.add_user()
     # user.login()
+    login_prompt = input("would you like to log in? y/n ")
+    if login_prompt is 'y':
+        username = input("username: ")
+        password = input("password: ")
+        isLoggedIn = user.login(username, password)
+        if isLoggedIn:
+            message = input("Add Scores ")
+        else:
+            
+            return False
+    else:
+        print("bye!")
+        return 0
 
 if __name__ == '__main__':
     main()
--- team_angela.py
+++ team_angela.py
@@ -14,10 +14,11 @@ class User:
         self.users.append(user)
         print('User {} successfully added'.format(self.username))
 
-    def login(self):
+    def login(self, username, password):
 
         for user in self.users:
-            if user['username'] == self.username and user['password'] == self.password:
+            if user['username'] == username and user['password'] == password:
+                print("{} has been logged in.".format(username))
                 return True
             return False
             
